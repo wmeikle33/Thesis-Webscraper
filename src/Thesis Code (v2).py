c8a6e0cf8bd898cb955cd1c893be162435f14e85
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-11-14 20:05:47 +0800

    Update Thesis Code (v2).py

--- src/Thesis Code (v2).py
+++ src/Thesis Code (v2).py
@@ -18,19 +18,6 @@ def create_new_file(path):
     f.write("")
     f.close()
 
-def make_driver(s: Settings) -> webdriver.Chrome:
-    opts = Options()
-    if s.headless:
-        opts.add_argument("--headless=new")
-    opts.add_argument("--disable-gpu")
-    opts.add_argument("--no-sandbox")
-    opts.add_argument("--window-size=1400,900")
-    PATH = '/Users/williammeikle/Downloads/chromedriver 7'
-    driver = webdriver.Chrome(PATH, options=opts)
-    driver.set_page_load_timeout(s.page_timeout_s)
-    return driver
-
-
 def get_details(directory, url):
     driver.get(url)
     title = driver.find_element_by_class_name('post-title')
