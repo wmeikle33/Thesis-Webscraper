ab6b2e9578a5a31c8311c7ea6efaaa4455ddfc98
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-02-14 23:46:55 +0800

    Update browser.py

--- src/browser.py
+++ src/browser.py
@@ -1,3 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 def make_driver(s: Settings) -> webdriver.Chrome:
     opts = Options()
     if s.headless:
@@ -5,7 +8,7 @@ def make_driver(s: Settings) -> webdriver.Chrome:
     opts.add_argument("--disable-gpu")
     opts.add_argument("--no-sandbox")
     opts.add_argument("--window-size=1400,900")
-    PATH = '/Users/williammeikle/Downloads/chromedriver 7'
-    driver = webdriver.Chrome(PATH, options=opts)
+
+    driver = webdriver.Chrome(options=opts)
     driver.set_page_load_timeout(s.page_timeout_s)
     return driver
