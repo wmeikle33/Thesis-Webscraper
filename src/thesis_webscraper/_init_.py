d3895f9fb62b8ef0550c4bcb20ad2a58ce6a19f3
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-02-25 00:25:53 +0800

    Update _init_.py

--- src/thesis_webscraper/_init_.py
+++ src/thesis_webscraper/_init_.py
@@ -1,10 +1,2 @@
-from selectorlib import Extractor
-import os
-import csv
-import re
-import json
-import scrapy
-import collections
-import xlsxwriter
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+__all__ = []
+__version__ = "0.1.0"
